e69e91aec9d15b02aae828abe58fa7fccfcb0865
haybeeudosen <aibeeudosen@gmail.com>
2020-08-17 10:18:44 +0100

    added files

---
+++ Documents/Games/js/index.js
@@ -0,0 +1,15 @@
+let name;
+name = prompt("What's your name");
+document.getElementById("text").innerHTML = "Hello" + " " + name + "!";
+
+
+// function bar(){
+//     document.getElementById("overlay").Value.innerHTML;
+// }
+function openNav() {
+    document.getElementById("menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("menu-content1").style.width = "0";
+}
---
+++ Documents/Games/js/index1.js
@@ -0,0 +1,35 @@
+// read;
+
+// function check(){
+//     read= document.getElementById("quiz").value;
+//     document.getElementById("quiz").innerHTML="The answer is b"
+// }
+
+
+
+// let q1;
+// function check(){
+    
+//     q1=document.getElementById("quiz").Q1.value;
+//     if (q1=="c. World Health Organization"){
+//         document.getElementById(quiz).innerHTML="you are correct";
+    
+//     };
+// }
+
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container')
+// startButton.addEventListener('click', startGame)
+
+function startGame(){
+    console.log('started')
+    // startButton.classList.add('hide')
+    // questionContainerElement.classList.remove('hide')
+}
+function setnextQuestion(){
+
+
+}
+function selectAnswer(){
+
+}
